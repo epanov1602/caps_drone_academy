dcaaeb293eab788289e0bc25c19aabd5bea2f2ab
Gene Panov <gene.panov@gmail.com>
2024-12-09 11:37:05 -0800

    Simple drone light show code

--- light_show.py
+++ light_show.py
@@ -5,31 +5,40 @@ from time import sleep
 drone = Tello()
 drone.connect()
 
-# take off and then wait for someone to press enter
-drone.send_rc_control(0, 0, 0, 0)
-drone.takeoff()
 
-print("Press Enter, before drone draws three squares")
-pressed = input()
+# which functions do we need in light show? start with these
+def fly_square(length):
+    drone.move_forward(length)
+    drone.move_right(length)
+    drone.move_back(length)
+    drone.move_left(length)
 
-drone.send_rc_control(0, 0, 100, 0)
-sleep(0.8)
-drone.send_rc_control(0, 0, 0, 0)
-sleep(0.2)
 
-#  -- move forward 25 cm?
-#drone.move_forward(25)
+def fly_circles(seconds, velocity):
+    drone.send_rc_control(0, velocity, 0, 100)
+    sleep(seconds)
 
-#  -- and do three squares of size 50 centimeters?
-#for lap in range(3):
-#    drone.move_forward(50)
-#    drone.move_right(50)
-#    drone.move_back(50)
-#    drone.move_left(50)
+def go_up(seconds):
+    drone.send_rc_control(0, 0, 100, 0)
+    sleep(seconds)
+    drone.send_rc_control(0, 0, 0, 0)
+    sleep(0.2)
+
+
+# wait for someone to press enter?
+#print("Press Enter, before drone draws three squares")
+#pressed = input()
+
+
+#  -- go up for 0.8 seconds?
+go_up(0.8)
+
+#  -- do three squares of size 50 centimeters?
+for lap in range(3):
+    fly_square(50)
 
 #  -- maybe fly some circles for 6 seconds
-drone.send_rc_control(0, 100, 0, 100)
-sleep(6)
+fly_circles(seconds=6.0, velocity=50)
 
 #  -- stop the movement and land
 drone.send_rc_control(0, 0, 0, 0)
